a595efe878a96173a1fb989761072eb76059d076
xeekst <xeekst@outlook.com>
2021-01-12 23:29:09 +0800

    提交novels的demo代码

---
+++ Practice.App.Xaml/Novels/Novels/Views/NovelContent.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Novels.Views
+{
+    public partial class NovelContent : ContentPage
+    {
+        public NovelContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
