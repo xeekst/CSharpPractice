9a8061e3a57d04e77f32e5405e7e11bbf0032493
xeekst <xeekst@outlook.com>
2021-01-23 01:18:58 +0800

    修改模板整体的模板，修改listview的DataTemplate

--- Practice.App.Xaml/Novels/Novels/Views/NovelListPage.xaml.cs
+++ Practice.App.Xaml/Novels/Novels/Views/NovelListPage.xaml.cs
@@ -10,6 +10,18 @@ namespace Novels.Views
         public NovelListPage()
         {
             InitializeComponent();
+            listView.ItemsSource = new[] { "a", "b", "c" };
+        }
+
+        void OnItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return; // has been set to null, do not 'process' tapped event
+            DisplayAlert("Tapped", e.SelectedItem + " row was tapped", "OK");
+            ((ListView)sender).SelectedItem = null; // de-select the row
+        }
+
+        void OnCellClicked(System.Object sender, System.EventArgs e)
+        {
         }
     }
 }
