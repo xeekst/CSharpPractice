47821663f9f39d07dfe39497f22c6337908096d3
xeekst <xeekst@outlook.com>
2021-02-09 23:27:58 +0800

    字体注入设置

--- Practice.App.Xaml/Novels/Novels/AssemblyInfo.cs
+++ Practice.App.Xaml/Novels/Novels/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+[assembly: ExportFont("OpenSans-Light.ttf", Alias = "OpenSans-Light")]
